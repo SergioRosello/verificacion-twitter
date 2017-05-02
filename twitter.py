1073c5cb21d4f316c26a914ef3fab4b4a436aadf
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-02 13:15:45 +0200

    Finished twitter optional exercise

--- twitter.py
+++ twitter.py
@@ -1,11 +1,18 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.keys import Keys
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
-    driver.get("http://www.google.es")
-    elem = driver.find_element_by_name("q")
-    elem.send_keys("u-tad")
-    elem.send_keys(Keys.RETURN)
+    driver.get("http://www.twitter.com")
+    login = driver.find_element_by_id('signin-email')
+    login.send_keys("@login_prueba")
+    password = driver.find_element_by_id('signin-password')
+    password.send_keys('verificacion2017')
+    password.send_keys(Keys.ENTER)
+    driver.set_page_load_timeout(2)
+    tweet = driver.find_element_by_id('tweet-box-home-timeline')
+    tweet.send_keys('Hi. I\'m using Selenium.')
+    tweet.send_keys(Keys.CONTROL, Keys.ENTER)
+    time.sleep(5)
     driver.close()
-    print 'Hello world!'
